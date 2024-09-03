7bf7735d3d7a3b06b9bcb4ae68ce397a6976dd50
Mahesh8867 <maheshkyadav158@gmail.com>
2024-09-03 18:39:22 +0530

    users saved to database

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,status,Depends
+from datbase import SessionLocal
 from models import Users
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from passlib.context import CryptContext
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+
 class CreateUserRequest(BaseModel):
     username: str
     email: str
@@ -9,16 +14,23 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
-
-@router.post("/auth/")
-async def create_user(create_user_request:CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+@router.post("/auth",status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request:CreateUserRequest,db: Session = Depends(get_db)):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
